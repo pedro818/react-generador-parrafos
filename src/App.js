cc3c29d36c202ea91638645f3b7b27ca249a6417
Pedrocode <pedro_cuello101@hotmail.com>
2021-02-08 15:43:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import data from './data';
+
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if(count <= 0){
+      amount = 1
+    }
+    if(count > 8){
+      amount = 8
+    }
+    setText(data.slice(0, amount));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <section className="section-center">
+      <h3>Generador de parrafos</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">
+            paragraphs:
+        </label>
+        <input type="number" name="amount" id="amount" 
+        value={count} onChange={(e)=> setCount(e.target.value)} 
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+        </input>
+          <button type="submit" className="btn">generate</button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+      </article>
+    </section>
+
   );
 }
 
